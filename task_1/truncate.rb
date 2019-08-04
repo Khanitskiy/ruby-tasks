20cf5eb9eb540ff6944b55de874c650bdbf0bd99
khanitskiy <khanitskiy@gmail.com>
2019-08-04 19:27:01 +0300

    completed

--- task_1/truncate.rb
+++ task_1/truncate.rb
@@ -1,2 +1,11 @@
 module Truncate
+
+  def truncate(*args, length: 120)
+    args.each do |thing|
+      define_method "#{thing}" do
+        variable = self.instance_variable_get("@#{thing}")
+        variable.length > length ? variable[0, length].delete("\n") << '...' : variable
+      end
+    end
+  end
 end
--- task_2/array.rb
+++ task_2/array.rb
@@ -1,2 +1,17 @@
 class Array
+
+  def rebuild!
+    size = self.count
+    self.each_with_index do |n, i|
+      self[i] = n.map { |el| yield(*get_params(el)) } if n.class == Array
+      self[i] = yield(*get_params(n))                 if n.class == Integer
+    end
+    self
+  end
+
+  private
+
+  def get_params(el)
+    self.count == 1 ? el : [el, self.count]
+  end
 end
